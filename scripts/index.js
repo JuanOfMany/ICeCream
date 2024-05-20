73c33628a2914f892239e113515943c39edb9b35
Juan.Pinol <juan.pinol@cvent.com>
2024-05-20 12:01:40 -0400

    did a new exercise in here.

---
+++ scripts/index.js
@@ -0,0 +1,39 @@
+"use strict"
+
+function handleEverything(event) {
+    console.log(event.target)
+}
+
+function handleCup(event) {
+    let toppings = document.getElementById("toppings");
+    console.log(event)
+    if (toppings.style.display === 'none') {
+        toppings.style.display = "block"
+    } else {
+        toppings.style.display = "none"
+    }
+}
+
+function handleNumOfScoops(event) {
+    if (event.target.value)
+}
+
+let total = 0;
+
+window.addEventListener("load", () => {
+    console.log('hello')
+
+    let inputs = ['numOfScoops', 'cup', 'cone', 'sprinkles', 'whippedCream', 'hotFudge', 'cherry', 'submit']
+
+    for (let i = 0; i < inputs.length; i++) {
+        let input = document.getElementById(inputs[i])
+        if (inputs[i] === 'cup') {
+            input.addEventListener("change", handleCup)
+            break;
+        }
+        if (inputs[i] === 'numOfScoops') {
+            input.addEventListener("change", handleNumOfScoops)
+        }
+        input.addEventListener("click", handleEverything)
+    }
+})
